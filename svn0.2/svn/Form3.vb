0763d7e018755e22e59ee42c0df54b8301c2be9d
Googllle <googlle.googlle19109@gmail.com>
2022-04-11 13:06:51 +0600

    part3

---
+++ svn0.2/svn/Form3.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form3
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim a As Object
+        a = Val(TextBox1.Text)
+        If a < -25 Then
+            Label2.Text = "Занятия в школе отменяются!"
+        Else
+            Label2.Text = "Можешь идти в школу. Желаю успехов!"
+        End If
+    End Sub
+End Class
